9ef8935e18050b62fc50861fbcd73fdb156c4153
Pavel Veretennikov <vermut@kid.lv>
2015-10-24 02:30:38 +0300

    limit state to something useful

--- src/org/traccar/web/client/model/StateReader.java
+++ src/org/traccar/web/client/model/StateReader.java
@@ -1,14 +1,11 @@
 package org.traccar.web.client.model;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import org.traccar.web.client.ApplicationContext;
 import org.traccar.web.shared.model.Position;
 
-import com.google.gwt.xml.client.Node;
-import com.google.gwt.xml.client.NodeList;
-import com.google.gwt.xml.client.XMLParser;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StateReader {
 
@@ -22,15 +19,20 @@ public class StateReader {
     public static List<StateItem> getState(Position position) {
         List<StateItem> state = new LinkedList<StateItem>();
 
-        state.add(new StateItem("valid", toString(position.getValid())));
-        state.add(new StateItem("time", ApplicationContext.getInstance().getFormatterUtil().getTimeFormat().format(position.getTime())));
+        state.add(new StateItem("login", toString(position.getDevice().getName())));
+        state.add(new StateItem("alive", position.getDevice().isActive() ? "alive" : "dead"));
+        state.add(new StateItem("age", toString(((int) (new Date().getTime() - position.getTime().getTime()) / 1000))));
         state.add(new StateItem("latitude", toString(position.getLatitude())));
         state.add(new StateItem("longitude", toString(position.getLongitude())));
+        state.add(new StateItem("time", ApplicationContext.getInstance().getFormatterUtil().getTimeFormat().format(position.getTime())));
+/*
         state.add(new StateItem("altitude", toString(position.getAltitude())));
         state.add(new StateItem("speed", ApplicationContext.getInstance().getFormatterUtil().getSpeedFormat().format(position.getSpeed())));
         state.add(new StateItem("course", toString(position.getCourse())));
         state.add(new StateItem("power", toString(position.getPower())));
         state.add(new StateItem("address", position.getAddress()));
+        state.add(new StateItem("valid", toString(position.getValid())));
+
 
         String other = position.getOther();
         if (other != null) {
@@ -42,7 +44,7 @@ public class StateReader {
                 }
             } catch (Exception error) {
             }
-        }
+        }*/
 
         return state;
     }
